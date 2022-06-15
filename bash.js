fb507e7d93a2bda85e25accc8831759f829ebc2a
Kevin <admin@kevins-mbp.mynetworksettings.com>
2022-06-15 16:48:32 -0400

    pwd functionality

---
+++ bash.js
@@ -0,0 +1,15 @@
+// import {cwd} from 'node:process';
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim();
+
+    if (cmd === 'pwd') {
+        process.stdout.write(process.cwd());
+    }else {
+        process.stdout.write('You typed: ' + cmd); 
+    }
+
+   
+    process.stdout.write('\nprompt > ');
+});
